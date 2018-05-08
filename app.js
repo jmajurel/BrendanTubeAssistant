9ed702da462a2afbcafac252fedd27edfee66268
j.majurel <j.majurel@bpp-tech.com>
2018-05-08 18:37:27 +0100

    update test message

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { dialogflow } = require('actions-on-google');
 const app = dialogflow();
 
 app.intent('tube_status', (conv, {tube_line}) => {
-  conv.close(`You requested the update for the tube line called ${tube_line}`); 
+  conv.close(`You requested the update for the tube called ${tube_line} line`); 
 });
 
 express().use(bodyParser.json(), app).listen(process.env.PORT)
