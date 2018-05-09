7200d766556f8f71c34d3a44cbebadbe9a3256ff
j.majurel <j.majurel@bpp-tech.com>
2018-05-09 13:10:43 +0100

    use request-promise-native

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ const tflAppId = 'a199d638';
 const tflAppKey = '4b9cf6ea343b37629649c8d7df2f2e3c';
 
 const app = dialogflow();
-const option = {
-  uri: `https://api.tfl.gov.uk/Line/${tube_line}/Status`,
-  qs: {
-    app_id: tflAppId,
-    app_key: tflAppKey
-  },
-  json: true
-};
+
 
 app.intent('tube_status', (conv, {tube_line}) => {
+ const option = {
+   uri: `https://api.tfl.gov.uk/Line/${tube_line}/Status`,
+   qs: {
+     app_id: tflAppId,
+     app_key: tflAppKey
+   },
+   json: true
+ };
  rp.request(option) 
    .then(function(tubeUpdate){
      let status = tubeUpdate[0].lineStatuses[0].statusSeverityDescription;
