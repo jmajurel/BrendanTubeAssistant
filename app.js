1e7f2c4bcc003d2ce9046f0376511deaf24e5fe1
j.majurel <jeanfelix.majurel@gmail.com>
2018-05-11 17:31:26 +0100

    refactoring app using object destructuring feature

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var brendan  = require('brendan');
+var helpers    = require('./helpers');
 
-const {dialogflow} = require('actions-on-google');
+const { dialogflow } = require('actions-on-google');
 const app = dialogflow();
 
-app.intent(brendan);
-
-/*app.intent('tube_status', (conv, {tube_line}) => {
-
- rpOption.uri = `/Line/${tube_line}/Status`;
-
- return rp(rpOption)
-   .then(function(body){
-     let [{lineStatuses}] = body;
-     let [{statusSeverityDescription}] = lineStatuses
-
-     conv.ask(`There is ${statusSeverityDescription} on the ${tube_line} line.
-	 Do you wish to know the status for any other line?`); 
-
-     conv.ask(new BasicCard({
-       title: `${tube_line} line Update`,
-       text: statusSeverityDescription,
-       buttons: new Button({
-	 title: 'tfl',
-	 url:'https://tfl.gov.uk/tube-dlr-overground/status/'
-       })
-     }));
-
-     conv.ask(new Suggestion('Do you wish to know the status of any other line?'));
-
-   })
-   .catch(function(err){
-     conv.ask(`Sorry I cannot get the status update for the ${tube_line} line, 
-	 Do you wish to know the status for any other line?`);
-   });
-});
-*/
+app.intent('tube_status', helpers.getStatusUpdate);
 
 express().use(bodyParser.json(), app).listen(process.env.PORT)
--- helpers/index.js
+++ helpers/index.js
@@ -11,23 +11,67 @@ const rpOption = {
   json: true
 };
 
-class tubeLine() {
-  constructor(name, status="", reason=""){
-    this.name = name;
-    this.status = status;
-    this.reason = reason;
-  }
+const conversationFallback = conv => {
+  conv.ask(`Sorry I cannot get the status update for the ${tube_line} line,
+      Do you wish to know the status for any other line?`);
 }
+
+const extractStatusUpdate = lines => {
+  return lines.reduce((acc, {name: tubeName, lineStatuses}) => {
+    let statuses = lineStatuses.reduce((acc, {statusSeverityDescription:statusDesc, reason=''})=>{
+      acc.push({statusDesc: statusDesc, reason: reason});
+      return acc;
+    },[]);
+    acc.push({name: tubeName, statuses: statuses});
+    return acc;
+  },[])
+};
+
+const dataPrepForConversation = linesUpdate => {
+
+  let delayedLines = linesUpdate.filter(lineUpdate => lineUpdate.statuses.filter({statusDesc, reason} => statusDesc === 'Good Service').length === 0)
+
+  if(delayedLines.length > 0){
+    delayedLines.reduce((acc, {name, statuses}) => {
+      statuses.forEach({statusDesc} => {
+	acc[statusDesc] += ` ${name}`; 
+      });
+      return acc;
+    },{});
+};
+
+const conversationResult = (delays, conv) => {
+  if(delays > 0) {
+    let sentence = delays.length > 1 ? 'there are ' : 'there is ';
+    for(let [key, val] of delays){
+      sentence += `${key} on ${val} `
+    }
+    conv.ask(sentence);
+  } else {
+    conv.ask('Good service on All lines');
+  }
+};
+
+const visualResult = (delays, conv) => {
+  conv.ask(new BasicCard({
+    title: `${tube_line} line Update`,
+    text: statusSeverityDescription,
+    buttons: new Button({
+      title: 'More on tfl website',
+      url:'https://tfl.gov.uk/tube-dlr-overground/status/'
+    })
+  }))
+};
 
 modulePackage.getStatusUpdate = () => {
 
   rpOption.uri = "/Line/Mode/tube/Status";
 
   return rp(rpOption)
-    .then(lines => { 
-      return [{name, lineStatus: [{statusSeverityDescription: status, reason} ]}]= [{lineStatus:[{reason=''}]}] = lines;
-    })
-    .then
+    .then(extractStatusUpdate)
+    .then(conversationResult)
+    .then(visualResult)
+    .catch(conversationFallback); 
 };
 
 module.exports = modulePackage;
