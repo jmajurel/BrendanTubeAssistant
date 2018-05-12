bad8c73950a62e0ba68e8ea3d3f6e716a8b5e0da
j.majurel <j.majurel@bpp-tech.com>
2018-05-12 18:27:45 +0100

    code review

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var helpers    = require('./helpers');
 const { dialogflow } = require('actions-on-google');
 const app = dialogflow();
 
-app.intent('tube_status', helpers.getStatusUpdate);
+app.intent('status_update', helpers.getStatusUpdate);
 
 express().use(bodyParser.json(), app).listen(process.env.PORT)
