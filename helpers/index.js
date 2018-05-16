e46eb91799688bd31043d947ecd7a1f3b4852cc4
j.majurel <jeanfelix.majurel@gmail.com>
2018-05-16 19:59:35 +0100

    daily commit

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const rp = require('request-promise-native');
+const {Table, Button} = require('actions-on-google');
 const {ssml} = require('./utils.js');
 
-const {Table, Button} = require('actions-on-google');
 const rpOption = {
   baseUrl: 'https://api.tfl.gov.uk/',
   qs: {
