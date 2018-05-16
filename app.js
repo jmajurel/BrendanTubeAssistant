5061a6f2e99815dc6c38185e583afa35c402c76b
j.majurel <jeanfelix.majurel@gmail.com>
2018-05-16 19:33:21 +0100

    try fixing exports

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-const {convStatusUpdates, convLines} = require('./helpers');
+const helpers = require('./helpers');
 
 const { dialogflow } = require('actions-on-google');
 const app = dialogflow();
 
-app.intent('status_updates', convStatusUpdates);
-app.intent('lines', convLines);
+app.intent('status_updates', helpers.convStatusUpdates);
+app.intent('lines', helpers.convLines);
 
 express().use(bodyParser.json(), app).listen(process.env.PORT)
