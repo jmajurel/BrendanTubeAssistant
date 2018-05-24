568720ad7aa58fdbf8a37e8cbb83a0f24d041775
j.majurel <jeanfelix.majurel@gmail.com>
2018-05-24 18:48:21 +0100

    add intent in package

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const brendan = require('./fulfillment');
 const app = dialogflow();
 
 app.intent('status_updates', brendan.statusUpdates); //UC1
-app.intent('journey', brendan.journey);
 app.intent('lines', brendan.lines);
+
+app.intent('journey', brendan.journey);
+app.intent('get_location', brendan.get_location);
 
 app.intent('Default Fallback Intent', brendan.defaultFallback);
 app.intent('Default Welcome Intent', brendan.welcome);
